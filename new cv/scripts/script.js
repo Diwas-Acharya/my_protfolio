d60a64da65b5df517ab80b6fbd3eee1aa265bfb8
Diwas Acharya <47555075+Diwas-Acharya@users.noreply.github.com>
2021-04-29 11:25:02 +0530

    Add files via upload

---
+++ new cv/scripts/script.js
@@ -0,0 +1,12 @@
+function display_sblock(){
+	document.getElementById("p_skills").style.display = "block";
+	document.getElementById("projects").style.display = "none";
+	// alert("jhkjfdgkjfh");
+}
+
+function display_pblock(){
+	document.getElementById("projects").style.display = "block";
+	document.getElementById("p_skills").style.display = "none";
+	// alert("jhkjfdgkjfh");
+}
+
